3f696e7c25fb320e8e97bd2b763018dd81c95499
Murat Yaşar <dev.muratyasar@gmail.com>
2026-03-02 09:25:42 +0100

    feat (actions-js): add logic to create PR

--- .github/actions/js-deps-update/main.js
+++ .github/actions/js-deps-update/main.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');  // To execute CLI scripts
+const exec = require('@actions/exec');      // To execute CLI scripts
+const github = require('@actions/github');  // To connect oktokit-API
 
 /*
 [X] 1. Parse Inputs
@@ -8,11 +9,11 @@ const exec = require('@actions/exec');  // To execute CLI scripts
       1.3 Github Token for authentication purposes (to create PRs)
       1.4 Working directory for which to check for dependencies
 [X] 2. Execute the 'npm update' command within the working directory
-[-] 3. Check whether there are modified package*.json files
-[-] 4. If there are modified files:
+[X] 3. Check whether there are modified package*.json files
+[X] 4. If there are modified files:
       4.1 Add and commit files to the target branch
       4.2 Create a PR to the base-branch using the octokit API (GitHub API)
-[-] 5. Otherwise, conclude the custom action
+[X] 5. Otherwise, conclude the custom action
 */
 
 // 1. Parse Inputs
@@ -22,11 +23,16 @@ const validateDirectoryName = ({directoryName}) => /^[a-zA-Z0-9_\-\.\/]+$/.test(
 
 async function run () {
   // The values should match with the action.yaml file!
-  const baseBranch = core.getInput('base-branch');
-  const targetBranch = core.getInput('target-branch');
-  const ghToken = core.getInput('gh-token');
-  const workingDir = core.getInput('working-directory');
+  const baseBranch = core.getInput('base-branch', {required: true}) || 'default-value';
+  const targetBranch = core.getInput('target-branch', {required: true});
+  const ghToken = core.getInput('gh-token', {required: true});
+  const workingDir = core.getInput('working-directory', {required: true});
   const debug = core.getBooleanInput('debug');
+
+  // Set common exec options
+  const commonExecOpts = {
+    cwd: workingDir   // cwd: current working directory
+  }
 
   // github-token should be set as secret for the security!
   core.setSecret(ghToken);
@@ -53,22 +59,53 @@ async function run () {
 
   // 2. Execute the 'npm update' command within the working directory
   await exec.exec('npm update', [], {
-    cwd: workingDir   // cwd: current working directory
+    ...commonExecOpts
   });
 
   const gitStatus = await exec.getExecOutput('git status -s package*.json', [], {
-    cwd: workingDir
+    ...commonExecOpts
   });
 
   // If the output of 'git status ...' command is zero, then there is no update
   if (gitStatus.stdout.length > 0) {
     core.info(`[js-deps-update]: There are updates available!`);
+    //TODO: Replace "gh-automation" with a repo-secret
+    await exec.exec(`git config --global user.name "gh-automation"`);
+    //TODO: Replace "gh-automation@mail.com" with a repo-secret
+    await exec.exec(`git config --global user.email "gh-automation@mail.com"`);
+    await exec.exec(`git checkout -b ${targetBranch}`, [], {
+      ...commonExecOpts
+    });
+    await exec.exec(`git add package-lock.json`, [], {
+      ...commonExecOpts
+    });
+    await exec.exec(`git commit -m "chor: update dependencies"`, [], {
+      ...commonExecOpts
+    });
+    //TODO: It's better to rebase first, and then to push, if everything goes well
+    await exec.exec(`git push -u origin ${targetBranch} --force`, [], {
+      ...commonExecOpts
+    });
+
+    const octokit = github.getOctokit(ghToken);
+    try {
+			// Create a PR
+			await octokit.rest.pulls.create({
+				owner: github.context.repo.owner,
+				repo: github.context.repo.repo,
+				title: "Update NPM Dependencies",
+				body: "This PR updates NPM packages",
+				base: baseBranch,
+				head: targetBranch,
+			});
+		} catch (e) {
+      core.error(`[js-deps-update]: Something went wrong while creating the PR! Check the logs below:`);
+      core.setFailed(e.message)
+      core.error(e);
+    }
   } else {
     core.info(`[js-deps-update]: There is no updates at the moment!`);
   }
-
-
-  core.info("I'm a custom JS action");
 }
 
 run();
