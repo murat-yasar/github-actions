b00204e24d54fbeac6a240e039a6cd56da2262ca
Murat Yaşar <dev.muratyasar@gmail.com>
2026-03-03 09:32:31 +0100

    refactor (actions-js): rename target-branch to head-branch

--- .github/actions/js-deps-update/main.js
+++ .github/actions/js-deps-update/main.js
@@ -5,16 +5,35 @@ const github = require('@actions/github');  // To connect oktokit-API
 /*
 [X] 1. Parse Inputs
       1.1 base-branch from which to check for updates
-      1.2 target-branch to use to create the PR
+      1.2 head-branch to use to create the PR
       1.3 Github Token for authentication purposes (to create PRs)
       1.4 Working directory for which to check for dependencies
 [X] 2. Execute the 'npm update' command within the working directory
 [X] 3. Check whether there are modified package*.json files
 [X] 4. If there are modified files:
-      4.1 Add and commit files to the target branch
+      4.1 Add and commit files to the head branch
       4.2 Create a PR to the base-branch using the octokit API (GitHub API)
 [X] 5. Otherwise, conclude the custom action
 */
+
+const setupGit = async () => {
+  //TODO: Replace "gh-automation" with a repo-secret
+  await exec.exec(`git config --global user.name "gh-automation"`);
+  //TODO: Replace "gh-automation@mail.com" with a repo-secret
+  await exec.exec(`git config --global user.email "gh-automation@mail.com"`);
+}
+
+const setupLogger = ({ debug, prefix } = { debug: false, prefix: '' }) => ({
+  debug: (message) => {
+    if (debug){ core.info(`DEBUG ${prefix}${prefix ? ' : ' : ''}${message}`); }
+  },
+  info: (message) => {
+    core.info(`${prefix}${prefix ? ' : ' : ''}${message}`);
+  },
+  error: (message) => {
+    core.error(`${prefix}${prefix ? ' : ' : ''}${message}`);
+  },
+});
 
 // 1. Parse Inputs
 // Branch-name validation for security
@@ -24,10 +43,11 @@ const validateDirectoryName = ({directoryName}) => /^[a-zA-Z0-9_\-\.\/]+$/.test(
 async function run () {
   // The values should match with the action.yaml file!
   const baseBranch = core.getInput('base-branch', {required: true}) || 'default-value';
-  const targetBranch = core.getInput('target-branch', {required: true});
+  const headBranch = core.getInput('head-branch', {required: true});
   const ghToken = core.getInput('gh-token', {required: true});
   const workingDir = core.getInput('working-directory', {required: true});
   const debug = core.getBooleanInput('debug');
+  const logger = setupLogger({ debug, prefix: '[js-deps-update]'});
 
   // Set common exec options
   const commonExecOpts = {
@@ -37,13 +57,15 @@ async function run () {
   // github-token should be set as secret for the security!
   core.setSecret(ghToken);
 
+  logger.debug('Validating inputs: base-branch, head-branch, working-directory');
+
   if (!validateBranchName({branchName: baseBranch})) {
     core.setFailed('Invalid base branch name! Branch names should include only characters, numbers, hypens, underscores, dots, and forward slashes...');
     return;
   }
 
-  if (!validateBranchName({branchName: targetBranch})) {
-    core.setFailed('Invalid target branch name! Branch names should include only characters, numbers, hypens, underscores, dots, and forward slashes...');
+  if (!validateBranchName({branchName: headBranch})) {
+    core.setFailed('Invalid head branch name! Branch names should include only characters, numbers, hypens, underscores, dots, and forward slashes...');
     return;
   }
 
@@ -52,10 +74,11 @@ async function run () {
     return;
   }
 
-  core.info(`[js-deps-update]: base branch is ${baseBranch}`);
-  core.info(`[js-deps-update]: target branch is ${targetBranch}`);
-  core.info(`[js-deps-update]: working directory is ${workingDir}`);
+  logger.debug(`Base branch is {baseBranch}`);
+  logger.debug(`Head branch is {headBranch}`);
+  logger.debug(`Working directory is {workingDir}`);
 
+  logger.debug(`Checking for package updates`);
 
   // 2. Execute the 'npm update' command within the working directory
   await exec.exec('npm update', [], {
@@ -66,14 +89,16 @@ async function run () {
     ...commonExecOpts
   });
 
-  // If the output of 'git status ...' command is zero, then there is no update
+  // 3. If the output of 'git status ...' command is zero, then there is no update
   if (gitStatus.stdout.length > 0) {
-    core.info(`[js-deps-update]: There are updates available!`);
-    //TODO: Replace "gh-automation" with a repo-secret
-    await exec.exec(`git config --global user.name "gh-automation"`);
-    //TODO: Replace "gh-automation@mail.com" with a repo-secret
-    await exec.exec(`git config --global user.email "gh-automation@mail.com"`);
-    await exec.exec(`git checkout -b ${targetBranch}`, [], {
+    logger.debug(`There are updates available!`);
+    logger.debug(`Setting up git...`);
+
+    // 4.1 Add and commit files to the head branch
+    await setupGit();
+
+    logger.debug(`Commiting and pushing package*.json changes...`);
+    await exec.exec(`git checkout -b ${headBranch}`, [], {
       ...commonExecOpts
     });
     await exec.exec(`git add package-lock.json`, [], {
@@ -83,28 +108,33 @@ async function run () {
       ...commonExecOpts
     });
     //TODO: It's better to rebase first, and then to push, if everything goes well
-    await exec.exec(`git push -u origin ${targetBranch} --force`, [], {
+    await exec.exec(`git push -u origin ${headBranch} --force`, [], {
       ...commonExecOpts
     });
 
+    // 4.2 Create a PR
+    logger.debug(`Fetching octokit API...`)
     const octokit = github.getOctokit(ghToken);
+
     try {
-			// Create a PR
+      logger.debug(`Creating PR using head branch: ${headBranch}`);
 			await octokit.rest.pulls.create({
 				owner: github.context.repo.owner,
 				repo: github.context.repo.repo,
 				title: "Update NPM Dependencies",
 				body: "This PR updates NPM packages",
 				base: baseBranch,
-				head: targetBranch,
+				head: headBranch,
 			});
 		} catch (e) {
-      core.error(`[js-deps-update]: Something went wrong while creating the PR! Check the logs below:`);
+      logger.error(`Something went wrong while creating the PR! Check the logs below:`);
       core.setFailed(e.message)
-      core.error(e);
+      logger.error(e);
     }
+
   } else {
-    core.info(`[js-deps-update]: There is no updates at the moment!`);
+    // 5. Conclude the custom action
+    logger.info(`There is no updates at the moment!`);
   }
 }
 
